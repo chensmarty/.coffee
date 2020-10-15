4b4c9bc8f255776841b35a05883eef0d27b103a1
chensmarty <72903126+chensmarty@users.noreply.github.com>
2020-10-15 17:28:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+const demo=document.getElementById("demo");
+const title=demo.innerHTML;
+const charArray=title.split('');
+let animationHTML='';
+charArray.forEach((char,index) => {
+  animationHTML=animationHTML+`<div class="char" style='animation-delay:${index*2}s'>${char}</div>`;
+})
+demo.innerHTML=animationHTML;
+
 /* Demo purposes only */
 $(".hover").mouseleave(
-  function () {
+  function() {
     $(this).removeClass("hover");
   }
 );
